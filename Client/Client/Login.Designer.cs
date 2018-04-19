f37b07e666bc9d8504bf9b95c493fb8584514b7d
Abdul Rafay <abdulrafe223@gmail.com>
2018-04-19 10:25:39 +0500

    Add Client

---
+++ Client/Client/Login.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Client
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lnkPassword = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lnkRegister = new System.Windows.Forms.LinkLabel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsernsme = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lnkPassword
+            // 
+            this.lnkPassword.AutoSize = true;
+            this.lnkPassword.Location = new System.Drawing.Point(106, 141);
+            this.lnkPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lnkPassword.Name = "lnkPassword";
+            this.lnkPassword.Size = new System.Drawing.Size(53, 13);
+            this.lnkPassword.TabIndex = 11;
+            this.lnkPassword.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Username";
+            // 
+            // lnkRegister
+            // 
+            this.lnkRegister.AutoSize = true;
+            this.lnkRegister.Location = new System.Drawing.Point(350, 287);
+            this.lnkRegister.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lnkRegister.Name = "lnkRegister";
+            this.lnkRegister.Size = new System.Drawing.Size(46, 13);
+            this.lnkRegister.TabIndex = 9;
+            this.lnkRegister.TabStop = true;
+            this.lnkRegister.Text = "Register";
+            this.lnkRegister.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRegister_LinkClicked);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(318, 221);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(86, 25);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(252, 145);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(152, 20);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // txtUsernsme
+            // 
+            this.txtUsernsme.Location = new System.Drawing.Point(252, 100);
+            this.txtUsernsme.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUsernsme.Name = "txtUsernsme";
+            this.txtUsernsme.Size = new System.Drawing.Size(152, 20);
+            this.txtUsernsme.TabIndex = 6;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 400);
+            this.Controls.Add(this.lnkPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lnkRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsernsme);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lnkPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel lnkRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsernsme;
+    }
+}
---
+++ Client/Client/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Client.WebReference;
+
+namespace Client
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            Service1 s = new Service1();
+            bool valid;
+            bool check;
+            s.Isvalid(txtUsernsme.Text, txtPassword.Text, out valid, out check);
+            if (valid)
+            {
+                MessageBox.Show("logged in");
+            }
+            else
+            {
+                MessageBox.Show("invalid");
+            }
+        }
+
+        private void lnkRegister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            Register r = new Register();
+            r.Show();
+            this.Hide();
+        }
+    }
+}
