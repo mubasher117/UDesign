60a240ff683f1de1586884ad9661554c354339b9
NausherwanFawad <shero997@gmail.com>
2018-04-19 08:49:52 +0500

    Add Client

---
+++ Client/Client/Register.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Web.Services;
+using Client.WebReference;
+
+namespace Client
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+
+            Service1 service = new Service1();
+            service.Register(txtUsername.Text, txtPassword.Text);
+            MessageBox.Show("registered");
+
+
+
+           
+        }
+
+        private void lnkLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
