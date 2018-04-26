0c03910b3454696ec9badedd66f6396906157f8f
daimjamil <daimjameel@gmail.com>
2018-04-26 10:42:48 +0500

    added admin and order dl class

---
+++ Server/Server/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server
+{
+    public class Admin
+    {
+
+        private string name = "mushi";
+        private string password = "admin";
+        public string Name { get => name; set => name = value; }
+        public string Password { get => password; set => password = value; }
+
+    }
+}
---
+++ Server/Server/OrderDL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server
+{
+    public class OrderDL
+    {
+
+        public static List<Order> orders = new List<Order>();
+
+        public void AddToCArt(Order purchase)
+        {
+            orders.Add(purchase);
+        }
+        public List<Order> DownloadOrdersList()
+        {
+            return orders;
+        }
+    }
+}
