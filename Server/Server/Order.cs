113da38865e1f1d395263d5aab6ea3a2f020fa3f
Mubasher Ahmad <mubasherahmad012345@gmail.com>
2018-04-26 08:05:52 +0500

    Added order class

---
+++ Server/Server/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace Server
+{
+    public class Order
+    {
+        private string name;
+        private string address;
+        private DateTime deliveryDate;
+        private Image design;
+
+        public string Name { get => name; set => name = value; }
+        public string Adress { get => address; set => address = value; }
+        public DateTime DeliveryDate { get => deliveryDate; set => deliveryDate = value; }
+        public Image Design { get => design; set => design = value; }
+    }
+}
